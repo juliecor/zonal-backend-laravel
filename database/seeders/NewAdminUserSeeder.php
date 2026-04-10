f13babe450a9fbecd2325fa5194a0bb44ab413cb
juliecor <repompojuliecor@gmail.com>
2026-04-10 17:55:50 +0800

    new updates

--- database/seeders/NewAdminUserSeeder.php
+++ database/seeders/NewAdminUserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class NewAdminUserSeeder extends Seeder
 {
